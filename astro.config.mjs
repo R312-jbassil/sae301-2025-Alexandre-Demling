d7836a1c09506b5d46c08494c9eabb921d7e64bc
Alexandre-Demling <alexandredemling@gmail.com>
2025-11-05 16:57:09 +0100

    SSR: add @astrojs/node adapter + output: server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
   integrations: [tailwind()],
 });
